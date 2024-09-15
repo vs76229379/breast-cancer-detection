f2879d20c26b8b460d919700e59572e08bc41359
vs76229379 <104732165+vs76229379@users.noreply.github.com>
2024-09-15 13:53:23 +0530

    new5

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def predict():
     return "Method not allowed", 405
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=False)
